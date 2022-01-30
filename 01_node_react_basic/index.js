8329542f171a08376533ca7350f5f4fa57c76c20
KangCine <kwg940930@naver.com>
2022-01-30 20:05:42 +0900

    bodyparser 설치 및 깃이그노어 수정

--- 01_node_react_basic/index.js
+++ 01_node_react_basic/index.js
@@ -1,16 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const mongoose = require('mongoose')
+const { User } = require('./model.User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://cine:cine@cluster0.7sr4t.mongodb.net/myFirstDatabase')
 .then(() => console.log('MongoDB Connected...'))
 .catch(arr => console.log(arr))
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!')
+  res.send('Hello, Express!');
 })
+
+app.post('/register', (req, res) => {
+  // 회원가입에 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err});
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
-})
+});
