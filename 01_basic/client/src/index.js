e09f4c77702cd396e11b3f27e7ec3f8c5ae83dee
강원기/Cine <kwg940930@gmail.com>
2022-02-01 22:50:49 +0900

    로그인 페이지 구현

--- 01_basic/client/src/index.js
+++ 01_basic/client/src/index.js
@@ -14,11 +14,12 @@ const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider 
-      store={createStoreWithMiddleware(Reducer, 
-          window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-        )}>
+    <Provider
+      store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
       <App />
     </Provider>
   </React.StrictMode>,
