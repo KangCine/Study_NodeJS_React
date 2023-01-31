c5483e49f4a6c0fc80204198792cc2320f94b612
KangCine <kwg940930@gmail.com>
2023-01-31 21:10:13 +0900

    회원가입 초간단 구현

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,7 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 6000;
+const express = require('express');
+const app = express();
+const port = 7777;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://cine:cine@cluster0.7sr4t.mongodb.net/myFirstDatabase?appName=mongosh+1.6.2', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true,
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
