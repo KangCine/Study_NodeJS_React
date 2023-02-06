268b54f97cb1fe910864a23e52cd9faad07d63a0
KangCine <kwg940930@gmail.com>
2023-02-06 22:57:28 +0900

    로그인, 로그아웃 구현

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -10,7 +13,7 @@ const userSchema = mongoose.Schema({
         trim: true,
         unique: 1
     },
-    passowrd: {
+    password: {
         type: String,
         minlength: 5,
     },
@@ -30,6 +33,63 @@ const userSchema = mongoose.Schema({
         type: Number,
     }
 })
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+
+        // 비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+            // bcrypt.hash(plainPassword, slat, callback function)
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+
+    // plainPassword 1234567 암호화된 비밀번호 등등..
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return callback(err);
+        callback(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function (callback) {
+    var user = this;
+    // jsonwebtoken을 이용해서 토큰 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token
+    user.save(function (err, user) {
+        if (err) {
+            return callback(err)
+        }
+        callback(null, user);
+    })
+}
+
+userSchema.statics.findByToken = function (token, callback) {
+    var user = this;
+
+    // 토큰을 decode한다.
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 token과 db에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return callback(err);
+            callback(null, user);
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
